aa6a3058dc5784ce9d190a8709f374d9a21f2903
joao tiago viegas <joao.viegas@maersk.com>
2019-08-01 09:51:24 +0200

    bucket event is now triggering the lambda function

---
+++ src/config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const winston = require('winston');
+const commons = require('@jtviegas/jscommons').commons;
+
+const config_module = function(){
+
+    let config = {
+        STAGE_SCOPE: [ 'dev', 'test', 'prod']
+        , ENV_SCOPE: [ 'local', 'dev', 'test', 'prod']
+        , WINSTON_CONFIG: {
+            level: 'debug',
+            format: winston.format.combine(
+                winston.format.splat(),
+                winston.format.timestamp(),
+                winston.format.printf(info => {
+                    return `${info.timestamp} ${info.level}: ${info.message}`;
+                })
+            ),
+            transports: [new winston.transports.Console()]
+        }
+
+    };
+
+    const variables = [
+        'TENANT'
+        , 'STAGE'
+        , 'ENV'
+        , 'DB_ENDPOINT'
+        , 'DB_API_REGION'
+        , 'DB_API_VERSION'
+        , 'ENTITIES'
+        , 'DB_API_ACCESS_KEY_ID'
+        , 'DB_API_ACCESS_KEY'
+    ];
+
+    const defaults = [
+        null
+        , 'dev'
+        , 'local'
+        , null // 'http://localhost:8000'
+        , 'eu-west-1'
+        , '2012-08-10'
+        , null
+        , null
+        , null
+    ];
+
+    config = commons.configByEnvironment(config, variables, defaults, '_SCOPE', []);
+
+    console.log(config)
+    return config;
+    
+}();
+
+module.exports = config_module;
---
+++ src/index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const winston = require('winston');
+const config = require("./config");
+const logger = winston.createLogger(config['WINSTON_CONFIG']);
+//const service = require('@jtviegas/store-loader-service')(config);
+const ServerError = require('@jtviegas/jscommons').ServerError;
+
+exports.handler = (event, context, callback) => {
+    logger.info('[handler|in] (event: %s, context: %s)', JSON.stringify(event, null, 4), JSON.stringify(context, null, 2));
+
+    const done = (err, res) => callback( null, {
+        statusCode: err ? ( err.status ? err.status : 500 ) : 200,
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    });
+    
+    try {
+
+        /*
+        let stage = null;
+        for(let i=0; i<event.Records.length; i++){
+
+            let record = event.Records[i];
+            if( record.s3 && record.s3.bucket && record.s3.bucket.name === ENTITIES_BUCKET_NAME ){
+                if( record.s3.object.key === DEV_UPDATE_SIGNAL_FILE ) {
+                    stage = 'dev';
+                    break;
+                }
+                if (record.s3.object.key === PROD_UPDATE_SIGNAL_FILE) {
+                    stage = 'prod';
+                    break;
+                }
+            }
+
+        }
+
+
+        if( null === stage )
+            throw new ServerError("no stage defined", 400);
+        else {
+            if( -1 === config.STAGES.indexOf(stage) )
+                throw new ServerError(`wrong stage: "${stage}"`, 400);
+            service.load(stage, done);
+        }
+        */
+        done(null, {});
+    }
+    catch(error) {
+      done(error);
+    }
+    logger.info('[handler|out]');
+};
+
+/*
+var event = {
+    "Records": [
+        {
+            "eventVersion": "2.0",
+            "eventSource": "aws:s3",
+            "awsRegion": "eu-west-1",
+            "eventTime": "2018-11-21T18:00:22.624Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {
+                "principalId": "A8HL0WRYU0T0V"
+            },
+            "requestParameters": {
+                "sourceIPAddress": "194.88.4.145"
+            },
+            "responseElements": {
+                "x-amz-request-id": "50A535B76621EAC2",
+                "x-amz-id-2": "9eigH2IKYrt+rx5p8lmjtEknR/MHV5cHzqQ9gZFK26RubVHVdUAOS6GOzM9U5Cb9qA9OzLQBj2Q="
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "s4ePartsDevUpdateEvent",
+                "bucket": {
+                    "name": "parts.split4ever.com",
+                    "ownerIdentity": {
+                        "principalId": "A8HL0WRYU0T0V"
+                    },
+                    "arn": "arn:aws:s3:::parts.split4ever.com"
+                },
+                "object": {
+                    "key": "update.dev",
+                    "size": 0,
+                    "eTag": "d41d8cd98f00b204e9800998ecf8427e",
+                    "sequencer": "005BF59D3688C73978"
+                }
+            }
+        }
+    ]
+}
+
+exports.handler(event, null, (e,d)=>{console.log('DONE', e,d);})
+*/
